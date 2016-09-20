b088a776dad1ccf66afeab9d0d406abdb10a4ee0
volnova <nastya.volnova@gmail.com>
2016-09-21 00:00:19 +0300

    Adding class

--- newsnn.py
+++ newsnn.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
-
 import requests
-
 from datetime import datetime
 from lxml import html
+
+
+class News:
+    def __init__(self, subject, img, date):
+        self.subject = subject
+        self.img = img
+        self.date = datetime.strptime(date, '%Y/%m/%d %H:%M')
 
 
 def request_to_site():
     date_now = datetime.strftime(datetime.now(), "%Y/%m/%d")
     r = requests.get('http://newsnn.ru/news/{}/'.format(date_now))
+    print 'http://newsnn.ru/news/{}/'.format(date_now)
     root = html.fromstring(r.text)
     xpath_dict = {
-        'titles': root.xpath('.//img[@class="preview_picture"]/@title'),
-        'links': root.xpath('.//img[@class="preview_picture"]/@src'),
-        'dates': root.xpath('.//div[@class="news-list"]/div/div/span[@class="news-date-time"]/span/text()')
+        'news': './/div[@class="news-list"]/*/div[@class="news-item"]',
+        'subject': './/img[@class="preview_picture"]/@title',
+        'img': './/img[@class="preview_picture"]/@src',
+        'date': './/span[@class="news-date-time"]/span/text()'
     }
+    news_list = root.xpath(xpath_dict['news'])
+    del root
 
-    for i in xrange(len(xpath_dict["titles"])):
-        news = {
-            'subject': xpath_dict["titles"][i],
-            'img': 'http://newsnn.ru/{}'.format(xpath_dict["links"][i]),
-            'date': '{} {}'.format(date_now, xpath_dict["dates"][i])
-        }
-        print news
+    for item in news_list:
+        obj = News(subject=item.xpath(xpath_dict["subject"])[0],
+                   img='http://newsnn.ru/{}'.format(item.xpath(xpath_dict['img'])[0]),
+                   date='{} {}'.format(date_now, item.xpath(xpath_dict['date'])[0]))
+        print obj.__dict__
 
 request_to_site()
